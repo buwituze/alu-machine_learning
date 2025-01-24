7d81cde9e7a802eacf0ca7ae8abe5873bf1f084a
buwituze <b.rutagengw@alustudent.com>
2025-01-24 19:52:21 +0200

    Linear Algebra

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python3
+"""calculating the shape of a matrix using recursion"""
+
+
 def matrix_shape(matrix):
-    if not isinstance(matrix, list):
-        return []
-    return [len(matrix)] + matrix_shape(matrix[0])
+    """Calculating the matrix's shape"""
+    shape = []
+    while type(matrix) == list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
